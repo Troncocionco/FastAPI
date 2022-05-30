d7dbbbcbf05e13290d691c679f987856cb2e9fc1
Troncocionco <gia.tronchin@outlook.it>
2022-05-30 11:43:03 +0200

    first

---
+++ myapi.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, Path
+
+""" reate an endpoint for API connectivity
+This end will provide a reference point for our requests:
+GET
+POST
+UPDATE
+DELETE  """
+app = FastAPI()
+
+
+students = {
+    1: {
+        "name": "john",
+        "age": 18,
+        "class": "year 12"
+    }
+}
+
+#Specify the URL for the API method
+@app.get("/index")
+def home():
+    return {"name": "Caccola"}
+
+@app.get("/get-student/{student_id}")
+def get_student(student_id: int):
+    return students[student_id]
+
+
+
